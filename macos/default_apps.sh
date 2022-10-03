44803740583f9c61fb217ce8a19bc7452bc955df
Henry Miskin <henry@mytos.bio>
2022-10-03 14:42:26 +0100

    Update install files

--- macos/default_apps.sh
+++ macos/default_apps.sh
@@ -1,8 +1,10 @@
-
 # osascript -e 'id of app "Visual Studio Code"'
 
 VSCODE_APP_ID="com.microsoft.VSCode"
 VSCODE_EXTENSIONS=(
+    .cc
+    .cpp
+    .hpp
     .js
     .ts
     .log
@@ -13,14 +15,14 @@ VSCODE_EXTENSIONS=(
     .lock
 )
 
-associate_extensions(){
+associate_extensions() {
     for extension in ${extensions[@]}; do
         echo "use ${app_id} for ${extension} files"
         duti -s "${app_id}" "${extension}" all
     done
 }
 
-associate_all_extensions(){
+associate_all_extensions() {
     local app_id=${VSCODE_APP_ID}
     local extensions=${VSCODE_EXTENSIONS[@]}
     associate_extensions
